457dca58092862c4444dae09d18aa1ee0ca7d438
Carl Wicker <carlwicker@gmail.com>
2019-12-05 22:40:59 +0000

    add item to console

--- client/src/components/ListAddItem.js
+++ client/src/components/ListAddItem.js
@@ -5,19 +5,38 @@ class ListAddItem extends React.Component {
   constructor(props) {
     super(props);
     this.state = { value: "" };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert("OUTPUT: " + this.state.value);
+    event.preventDefault();
+  }
+
+  componentDidUpdate() {
+    console.log(this.state.value);
   }
 
   render() {
     return (
       <div id="list-item-add" className="input-group mb-3">
         <input
+          value={this.state.value}
+          onChange={this.handleChange}
           type="text"
           className="form-control"
           id="add-item-input"
-          defaultValue=""
         ></input>
         <div className="input-group-append">
-          <button className="btn btn-primary">Add Item</button>
+          <button className="btn btn-primary" onClick={this.handleSubmit}>
+            Add Item
+          </button>
         </div>
       </div>
     );
