b40f3575f501e74a5b53eabf8040f3c0d7e01e23
Carl Wicker <carlwicker@gmail.com>
2019-12-12 13:56:29 +0000

    delete button function

--- client/src/components/ListItem.js
+++ client/src/components/ListItem.js
@@ -1,15 +1,19 @@
 import React from "react";
-import Axios from "axios";
 import "./ListItem.css";
 
 function ListItem(props) {
   return (
     <tr>
       <th scope="row">{props._id}</th>
-      <td>{props.name} </td>
+      <td>{props.name}</td>
       <td>
         <button className="btn btn-primary">Edit</button>{" "}
-        <button className="btn btn-danger">Delete</button>
+        <button
+          onClick={() => console.log("Clicked: " + props._id)}
+          className="btn btn-danger"
+        >
+          Delete
+        </button>
       </td>
     </tr>
   );
