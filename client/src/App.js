c3ed138c3e304ef4b795f124a4cfb078c9ba4262
Carl Wicker <carlwicker@gmail.com>
2019-12-19 11:54:41 +0000

    cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -8,18 +8,15 @@ import Header from "./components/Header";
 function App() {
   let pageTitles = [
     { page: "Header", title: "Item List" },
-    { page: "ListItemAdd", title: "ListItemAdd Title from App props" },
-    { page: "List", title: "List Title from App props" },
     { page: "Footer", title: "Footer stuff..." }
   ];
 
   return (
     <div className="container">
       <Header title={pageTitles[0].title} />
-      <ListAddItem title={pageTitles[1].title} />
-      <List title={pageTitles[2].title} />
-
-      <Footer title={pageTitles[3].title} />
+      <ListAddItem />
+      <List />
+      <Footer title={pageTitles[1].title} />
     </div>
   );
 }
